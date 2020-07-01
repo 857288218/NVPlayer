3321f19e4f095f7efc6ba6f9d616435cd028b847
renjunqing <renjunqing@windimg.com>
2020-07-01 19:36:52 +0800

    first

---
+++ app/src/main/java/com/xiao/nicevieoplayer/example/RecyclerViewActivity.java
@@ -0,0 +1,83 @@
+package com.xiao.nicevieoplayer.example;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.xiao.nicevideoplayer.NiceVideoPlayer;
+import com.xiao.nicevideoplayer.NiceVideoPlayerManager;
+import com.xiao.nicevieoplayer.R;
+import com.xiao.nicevieoplayer.example.adapter.VideoAdapter;
+import com.xiao.nicevieoplayer.example.adapter.holder.VideoViewHolder;
+import com.xiao.nicevieoplayer.example.util.AutoPlayUtils;
+import com.xiao.nicevieoplayer.example.util.DataUtil;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+        init();
+    }
+
+    private void init() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        VideoAdapter adapter = new VideoAdapter(this, DataUtil.getVideoListData());
+        mRecyclerView.setAdapter(adapter);
+
+        mRecyclerView.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+            @Override
+            public void onChildViewAttachedToWindow(View view) {
+
+            }
+
+            @Override
+            public void onChildViewDetachedFromWindow(View view) {
+                NiceVideoPlayer niceVideoPlayer = (NiceVideoPlayer) view.findViewById(R.id.nice_video_player);
+                if (niceVideoPlayer == NiceVideoPlayerManager.instance().getCurrentNiceVideoPlayer()) {
+                    NiceVideoPlayerManager.instance().releaseNiceVideoPlayer();
+                }
+            }
+        });
+
+        //滑动自动播放
+//        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+//                    AutoPlayUtils.onScrollPlayVideo(recyclerView, R.id.nice_video_player, mLayoutManager.findFirstVisibleItemPosition(), mLayoutManager.findLastVisibleItemPosition());
+//                }
+//            }
+//
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//                if (dy != 0) {
+//                    AutoPlayUtils.onScrollReleaseAllVideos(mLayoutManager.findFirstVisibleItemPosition(), mLayoutManager.findLastVisibleItemPosition(), 0.2f);
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        NiceVideoPlayerManager.instance().releaseNiceVideoPlayer();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (NiceVideoPlayerManager.instance().onBackPressd()) return;
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/xiao/nicevieoplayer/example/util/AutoPlayUtils.java
@@ -0,0 +1,78 @@
+package com.xiao.nicevieoplayer.example.util;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.xiao.nicevideoplayer.INiceVideoPlayer;
+import com.xiao.nicevideoplayer.NiceVideoPlayer;
+import com.xiao.nicevideoplayer.NiceVideoPlayerManager;
+
+/**
+ * 列表自动播放工具类
+ *
+ * @author Liberations
+ */
+public class AutoPlayUtils {
+    public static int positionInList = -1;//记录当前播放列表位置
+
+    private AutoPlayUtils() {
+    }
+
+    /**
+     * @param firstVisiblePosition 首个可见item位置
+     * @param lastVisiblePosition  最后一个可见item位置
+     */
+    public static void onScrollPlayVideo(RecyclerView recyclerView, int jzvdId, int firstVisiblePosition, int lastVisiblePosition) {
+        for (int i = 0; i <= lastVisiblePosition - firstVisiblePosition; i++) {
+            View child = recyclerView.getChildAt(i);
+            View view = child.findViewById(jzvdId);
+            if (view != null && view instanceof NiceVideoPlayer) {
+                NiceVideoPlayer player = (NiceVideoPlayer) view;
+                if (getViewVisiblePercent(player) == 1f) {
+                    if (positionInList != i + firstVisiblePosition) {
+                        if (player.isIdle()) {
+                            player.start();
+                        } else {
+                            player.restart();
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param firstVisiblePosition 首个可见item位置
+     * @param lastVisiblePosition  最后一个可见item位置
+     * @param percent              当item被遮挡percent/1时释放,percent取值0-1
+     */
+    public static void onScrollReleaseAllVideos(int firstVisiblePosition, int lastVisiblePosition, float percent) {
+        if (NiceVideoPlayerManager.instance().getCurrentNiceVideoPlayer() == null) return;
+        if (positionInList >= 0) {
+            if ((positionInList <= firstVisiblePosition || positionInList >= lastVisiblePosition - 1)) {
+                if (getViewVisiblePercent(NiceVideoPlayerManager.instance().getCurrentNiceVideoPlayer()) < percent) {
+                    NiceVideoPlayerManager.instance().releaseNiceVideoPlayer();
+                }
+            }
+        }
+    }
+
+    /**
+     * @param view
+     * @return 当前视图可见比列
+     */
+    public static float getViewVisiblePercent(View view) {
+        if (view == null) {
+            return 0f;
+        }
+        float height = view.getHeight();
+        Rect rect = new Rect();
+        if (!view.getLocalVisibleRect(rect)) {
+            return 0f;
+        }
+        float visibleHeight = rect.bottom - rect.top;
+        return visibleHeight / height;
+    }
+}
