42c9726b29bbbb1bb49e0612a7b5c202e5ba664a
renjunqing <renjunqing@windimg.com>
2020-07-04 15:07:17 +0800

    Update INiceVideoPlayer.java

--- nicevideoplayer/src/main/java/com/xiao/nicevideoplayer/INiceVideoPlayer.java
+++ nicevideoplayer/src/main/java/com/xiao/nicevideoplayer/INiceVideoPlayer.java
@@ -10,61 +10,61 @@ public interface INiceVideoPlayer {
     /**
      * 播放错误
      **/
-    public static final int STATE_ERROR = -1;
+    int STATE_ERROR = -1;
     /**
      * 播放未开始
      **/
-    public static final int STATE_IDLE = 0;
+    int STATE_IDLE = 0;
     /**
      * 播放准备中
      **/
-    public static final int STATE_PREPARING = 1;
+    int STATE_PREPARING = 1;
     /**
      * 播放准备就绪
      **/
-    public static final int STATE_PREPARED = 2;
+    int STATE_PREPARED = 2;
     /**
      * 正在播放
      **/
-    public static final int STATE_PLAYING = 3;
+    int STATE_PLAYING = 3;
     /**
      * 暂停播放
      **/
-    public static final int STATE_PAUSED = 4;
+    int STATE_PAUSED = 4;
     /**
      * 正在缓冲(播放器正在播放时，缓冲区数据不足，进行缓冲，缓冲区数据足够后恢复播放)
      **/
-    public static final int STATE_BUFFERING_PLAYING = 5;
+    int STATE_BUFFERING_PLAYING = 5;
     /**
      * 正在缓冲(播放器正在播放时，缓冲区数据不足，进行缓冲，此时暂停播放器，继续缓冲，缓冲区数据足够后恢复暂停
      **/
-    public static final int STATE_BUFFERING_PAUSED = 6;
+    int STATE_BUFFERING_PAUSED = 6;
     /**
      * 播放完成
      **/
-    public static final int STATE_COMPLETED = 7;
+    int STATE_COMPLETED = 7;
 
     /**
      * 普通模式
      **/
-    public static final int MODE_NORMAL = 10;
+    int MODE_NORMAL = 10;
     /**
      * 全屏模式
      **/
-    public static final int MODE_FULL_SCREEN = 11;
+    int MODE_FULL_SCREEN = 11;
     /**
      * 小窗口模式
      **/
-    public static final int MODE_TINY_WINDOW = 12;
+    int MODE_TINY_WINDOW = 12;
 
     /**
      * IjkPlayer
      **/
-    public static final int TYPE_IJK = 111;
+    int TYPE_IJK = 111;
     /**
      * MediaPlayer
      **/
-    public static final int TYPE_NATIVE = 222;
+    int TYPE_NATIVE = 222;
 
     /**
      * 设置视频Url，以及headers
